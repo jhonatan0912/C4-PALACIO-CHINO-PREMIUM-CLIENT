25f5a158832eecb1df76178db5995ae6cad5df27
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-02-18 16:16:09 -0500

    login component added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className='footer relative pt-2'>
+    <div className='footer relative py-3'>
       <div className="footer__line"></div>
       <div className='grid md:grid-cols-4 capitalize p-5'>
         <div>
@@ -25,18 +25,18 @@ const Footer = () => {
             <Link>mi información</Link>
           </nav>
         </div>
-        <div>
+        <div className='flex flex-col gap-3'>
           <b className='uppercase'>formas de pago</b>
           <div className='flex gap-2'>
-            <img src="/visa.svg" alt="visa"  width={50}/>
-            <img src="/mastercard.svg" alt="mastercard"  width={50}/>
-            <img src="/american.svg" alt="american"  width={50}/>
+            <img src="/visa.svg" alt="visa" width={50} />
+            <img src="/mastercard.svg" alt="mastercard" width={50} />
+            <img src="/american.svg" alt="american" width={50} />
 
           </div>
           <b className='uppercase'>siguenos</b>
           <div className='flex gap-2'>
-            <i className='fa fa-facebook text-3xl'></i>
-            <i className='fa fa-instagram text-3xl'></i>
+            <Link><i className='fa fa-facebook text-3xl'></i></Link>
+            <Link><i className='fa fa-instagram text-3xl'></i></Link>
           </div>
         </div>
       </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex flex-col gap-4 md:w-4/12 m-auto shadow-2xl rounded-lg p-10 md:my-24'>
+
+      <b className='text-center capitalize md:text-2xl'>bienvenido</b>
+      <label htmlFor="email">
+        <p>correo electronico</p>
+        <input type="text" placeholder='abc@abc.com' id='email' className='bg-zinc-300 p-2 w-full rounded-lg' />
+      </label>
+      <label htmlFor="password">
+        <p>contraseña</p>
+        <input type="password" placeholder='************' id='password' className='bg-zinc-300 p-2 w-full rounded-lg' />
+      </label>
+      <button className='red p-3 rounded-lg uppercase font-bold'>iniciar sesión</button>
+
+
+    </div>
   )
 }
 
